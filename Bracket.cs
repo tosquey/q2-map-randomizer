f2fbdf00d0f6773b939700fe2df1c035b4a12d5e
Luiz Mattos <luizegmattos@gmail.com>
2021-06-24 15:39:04 -0400

    v1

---
+++ Bracket.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace q2_map_randomizer
+{
+    public static class Bracket
+    {
+        public static List<Match> GetMatches(string file)
+        {
+            List<Match> matches = new List<Match>();
+
+            string contents = File.ReadAllText(file);
+            var json = JObject.Parse(contents);
+
+            var entrants = json["data"]["items"]["entities"]["entrants"].Select(a => new Player() {
+                Id = a["id"].Value<int>(),
+                Nick = a["mutations"]["players"].First.First["gamerTag"].Value<string>(),
+                Seed = a["initialSeedNum"].Value<int>()
+            });
+
+            var nulo = new Player() { Nick = "TBD" };
+
+            foreach (var node in json["data"]["items"]["entities"]["sets"])
+            {
+                string identifier = node["identifier"].Value<string>();
+
+                Player player1, player2;
+                int player1Id, player2Id = 0;
+
+                if (int.TryParse(node["entrant1Id"].Value<string>(), out player1Id))
+                {
+                    player1 = entrants.Where(a => a.Id == player1Id).FirstOrDefault();
+                }
+                else
+                {
+                    player1 = nulo;
+                }
+
+                if (int.TryParse(node["entrant2Id"].Value<string>(), out player2Id))
+                {
+                    player2 = entrants.Where(a => a.Id == player2Id).FirstOrDefault();
+                }
+                else
+                {
+                    player2 = nulo;
+                }
+
+                matches.Add(new Match() { Identifier = identifier, Players = { player1, player2 }});
+            }
+
+            return matches;
+        }
+    }
+
+    public class Match
+    {
+        public Match()
+        {
+            Players = new List<Player>();
+        }
+        public string Identifier {get; set; }
+        public List<Player> Players { get; set; }
+    }
+
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Nick { get; set; }
+        public int Seed { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace q2_map_randomizer
+{
+    class Program
+    {
+        static char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+        static void Main(string[] args)
+        {
+            var matches = Bracket.GetMatches("/Users/lmatto1/temp/bracket.json");
+
+            string file = string.Format("/Users/lmatto1/temp/maps-{0}.txt", DateTime.Now.ToString("yyyyMMddhhmmss"));
+
+            using (StreamWriter output = new StreamWriter(file, append: true))
+            {
+                foreach (var match in matches.OrderBy(a => a.Identifier))
+                {
+                    string matchMap = string.Format("Jogo {0}: {1} - {2}",
+                        match.Identifier, string.Format("{0} vs {1}", match.Players[0].Nick, match.Players[1].Nick),
+                        GetRandomMap());
+
+                    Console.WriteLine(matchMap);
+                    output.WriteLine(matchMap);
+                }
+            }
+        }
+
+        static List<string> GetMaps()
+        {
+            return new List<string>() {"q2dm1","q2dm3","match1","ztn2dm2","ztn2dm3","ptrip","q2rdm2","q2duel5"};
+        }
+
+        static string GetRandomMap()
+        {
+            return GetMaps().OrderBy(a => Guid.NewGuid()).First();
+        }
+
+        static string GetMatch(int index)
+        {
+            string matchName = string.Empty;
+            
+            string preffix = string.Empty;
+            string suffix = string.Empty;
+
+            int max = alpha.Length;
+            int leftover = (int)(Math.Floor((double)index / (double)max));
+            
+            if (leftover > 0)
+            {
+                preffix = alpha[leftover -1].ToString();
+            }
+
+            index = index - (max * leftover);
+            suffix = alpha[index].ToString();
+
+            matchName = preffix + suffix;
+
+            return matchName;
+        }
+
+        static int GetMatchCount(string latestRound)
+        {
+            int matchCount = 0;
+            
+            if (latestRound.Length == 1)
+            {
+                matchCount = Array.IndexOf(alpha, latestRound[0]) + 1;
+            }
+            else
+            {
+                int aux = (Array.IndexOf(alpha, latestRound[0]) + 1) * 26;
+                matchCount = Array.IndexOf(alpha, latestRound[1]) + 1 + aux;
+            }
+
+            return matchCount;
+        }
+    }
+}
